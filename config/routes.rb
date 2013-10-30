b2d92696c3a5c0619015ef77a1df07140073880d
James Martinez <jmar910@gmail.com>
2013-10-30 16:18:28 -0400

    configure high voltage route for root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NeoTestSite::Application.routes.draw do
+
+  root :to => 'high_voltage/pages#show', id: 'index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
