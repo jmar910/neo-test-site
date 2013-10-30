591bb524c0ac3dfe5a554c60a5a6fb0991cfc4fe
James Martinez <jmar910@gmail.com>
2013-10-30 16:18:23 -0400

    Website, styling, and supplemental js

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    //Quick function for contact form default text + behavior
+	function textFieldClear(selector){
+		var storeDefault;
+
+    	$(selector).focus(function(){
+            //Store the default value of the current selected input
+    		storeDefault = $(this).prop('defaultValue');
+
+            //On focus, check if current value is equal to default value
+            //If it is, clear placeholder and change styling for user input
+    		if($(this).val() == storeDefault){
+    			$(this).val('');
+    			$(this).css('color', 'white');
+    		}
+
+    		$(selector).blur(function(){
+                //If field is empty, revert back to default value and
+                //remove any styling that was added
+    			if($(this).val() == ""){
+    				$(this).val(storeDefault);
+    				$(this).removeAttr('style');
+				}
+    		});
+		});
+	}
+
+    //Quick and dirty image fader
+    function imageFader() {
+        var counter = 1;
+        setInterval(function(){
+            if (counter < 3){
+                //On reset loop after fadeIn of first image, un-hide second graphic
+                if ($('#iphone2').css('display') == 'none') {$('#iphone2').show()}
+
+                //Fade to next graphic
+                $('#iphone' + counter).fadeOut();
+                counter = counter + 1;
+            }
+            //Reset loop and go back to first image
+            else {
+                $('#iphone1').fadeIn();
+                counter = 1; 
+            }
+        }, 5000);
+    };
+
+    //Call functions for input and textarea 
+	textFieldClear('.contact-content input[type="text"]');
+	textFieldClear('.contact-content textarea');
+
+    //Call image fader function
+    imageFader();
+});
